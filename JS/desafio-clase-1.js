9fdfbb4b8c77fc2610455d538afb4be31d7ede9e
Zarate Agustin <zarateagustin3@gmail.com>
2021-10-31 10:49:41 -0300

    Desafio Clase 1

---
+++ JS/desafio-clase-1.js
@@ -0,0 +1,5 @@
+//Desafio clase 1
+let heladoChocolate = 80
+let pedido = parseInt(prompt("Bienvenido a nuestra heladeria, el helado de chocolate cuesta $" + heladoChocolate +  " , ¿Cuantos helados quiere llevar?"))
+let valorPedido = (heladoChocolate * pedido)
+alert ("el valor de su pedido es de " + valorPedido)
