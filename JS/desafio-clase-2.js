be893d773767e06778c3cbd92a3aa5544e4ec5b0
Zarate Agustin <zarateagustin3@gmail.com>
2021-10-31 11:27:44 -0300

    Desafio Clase 2

---
+++ JS/desafio-clase-2.js
@@ -0,0 +1,17 @@
+//Desafio clase 2
+let heladoChocolate = 80
+let stockHeladoCholoclate = 5
+let pedido = parseInt(prompt("Bienvenido a nuestra heladeria, el helado de chocolate cuesta $" + heladoChocolate +  " , ¿Cuantos helados quiere llevar?"))
+if (pedido <= stockHeladoCholoclate) {
+    let valorPedido = (heladoChocolate * pedido)
+alert ("el valor de su pedido es de " + valorPedido)
+stockHeladoCholoclate = stockHeladoCholoclate - pedido
+console.log (stockHeladoCholoclate) 
+} else {
+    alert ("lo sentimos pero no contamos con el stock necesario \n" +
+            "el stock actual es de " + stockHeladoCholoclate)
+}
+
+
+
+
