d9dee4a1ff10492bdd0595e0e8f582d755dc0ae8
Zarate Agustin <zarateagustin3@gmail.com>
2021-12-20 23:04:41 -0300

    3er Entrega Proyecto final

---
+++ simulador-truco/dom.js
@@ -0,0 +1,21 @@
+/* const preciosCamisetas = camisetas.filter(function (camiseta) {
+    return camiseta.precio > 0
+
+}); */
+
+//------------------------------------------------------//  //------- SIRVE PARA ORDENAR LOS ARRAYS SEGUN SU "UNICODE"-----//
+//-------------------- METODO .SORT -------------------//   //------- CON LOS N° Y OBJETOS SE DEBE DE USAR UNA FUNCION ----//
+//----------------------------------------------------//   //------ let numeros = [5, 1, 20, 65, 10]    |     let numerosOrdenados = numeros.sort(function(a, b) {return a-b}); ---//
+
+
+/* let preciosOrdenados = preciosCamisetas.sort(function(a, b) {
+    return a.precio - b.precio
+}); */
+
+for(const carta of mazo) {
+    $("#barajaEspañola").append(`
+        <div>
+            <img src="${carta.imagen}" class="presentation-card" alt="">
+        </div>
+    `)
+}
